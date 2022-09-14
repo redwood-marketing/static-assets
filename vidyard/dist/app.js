8adea2e54fab3ae5a9101d691fd15d835e94019b
Sebastián La Rosa <slarosa@advsyscon.com>
2022-09-14 19:19:59 -0300

    Respond cta message with user payload

--- vidyard/dist/app.js
+++ vidyard/dist/app.js
@@ -1,15 +1,5 @@
 "use strict";
 
-this != this.top && this.top.postMessage("From Frame", "https://videos.redwood.com");
-
-this.addEventListener("message", event => {
-    console.log(event);
-    if (event.origin !== "https://play.vidyard.com")
-        return;
-
-    event.source.postMessage("From Top", event.origin);
-});
-
 window.onVidyardAPI = (vidyardEmbed) => {
     vidyardEmbed.api.addReadyListener((_, player) => {
         fetch(`https://www.redwood.com/wp-json/utilities/vidyard/v1/users/${player.uuid}`)
@@ -25,6 +15,13 @@ window.onVidyardAPI = (vidyardEmbed) => {
                         <a class="rw-btn" href="https://${window.user.calendar_link}" target="_blank">Put Time On My Calendar →</a>
                     </div>
                 `;
+
+                window.addEventListener("message", event => {
+                    if (event.origin !== "https://play.vidyard.com")
+                        return;
+                
+                    event.source.postMessage(result.data, event.origin);
+                });
 
                 document.getElementById("year").innerHTML = new Date().getFullYear();
                 document.getElementById("main-content").appendChild(sidebar);
