973a09eb1aeee4d3c25df9b064318651b2e23a74
Sebastián La Rosa <slarosa@advsyscon.com>
2022-09-14 16:50:30 -0300

    Init postMessage communication bridge

--- vidyard/dist/app.js
+++ vidyard/dist/app.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log(new Date());
+
+this != this.top && this.top.postMessage("From Frame", "https://videos.redwood.com");
+
+this.addEventListener("message", event => {
+    console.log(event);
+    if (event.origin !== "https://play.vidyard.com")
+        return;
+
+    event.source.postMessage("From Top", event.origin);
+});
