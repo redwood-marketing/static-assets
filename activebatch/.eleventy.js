ec3a4b2e59a93699a6058dfedc64413d7fa3163c
Adam <adam.shen@redwood.com>
2024-03-08 16:36:46 -0800

    working njk templates

--- activebatch/.eleventy.js
+++ activebatch/.eleventy.js
@@ -1,29 +1,31 @@
-module.exports = config => {
+module.exports = (config) => {
 
-    const passThrough = [
-        "src/css/*.min.*",
-        "src/js/*.min.*",
-        "src/img",
-        "src/*.png",
-        "src/*.svg",
-        "src/*.ico",
-        "src/*.webmanifest",
-    ]
+	const setAttribute = (dictionary, key, value) => {
+		dictionary[key] = value;
+		return dictionary;
+	};
 
-    passThrough.forEach( item => {
-        config.addPassthroughCopy(item);
-    });
+	// Extend Nunjucks environment
+	config.addNunjucksAsyncFilter('setAttribute', (dictionary, key, value, callback) => {
+		const result = setAttribute(dictionary, key, value);
+		callback(null, result);
+	});
+	
+	const passThrough = ['src/css/*.min.*', 'src/js/*.min.*', 'src/img', 'src/*.png', 'src/*.svg', 'src/*.ico', 'src/*.webmanifest'];
 
-    config.addWatchTarget("src/**/*");
+	passThrough.forEach((item) => {
+		config.addPassthroughCopy(item);
+	});
 
-    return {
-        markdownTemplateEngine: 'njk',
-        dataTemplateEngine: 'njk',
-        htmlTemplateEngine: 'njk',
-        dir: {
-            input: "activebatch/src/pages",
-            output: "activebatch/dist/pages"
-        }
-    }
+	config.addWatchTarget('src/**/*');
 
-}
+	return {
+		markdownTemplateEngine: 'njk',
+		dataTemplateEngine: 'njk',
+		htmlTemplateEngine: 'njk',
+		dir: {
+			input: 'activebatch/src/pages',
+			output: 'activebatch/dist/pages',
+		},
+	};
+};
